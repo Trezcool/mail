2b3066e6976a41c8e5cf891502e3de0f0a5d04e1
Tresor Kambembo <tresor@propertyfinder.ae>
2023-03-24 08:54:54 +0400

    refactor console provider

--- console.go
+++ console.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"mime/multipart"
 	"net/mail"
@@ -76,7 +77,7 @@ func (p ConsoleProvider) sendMessage(msg *Message) error {
 	return nil
 }
 
-func (p ConsoleProvider) send(msg Message) error {
+func (p ConsoleProvider) send(msg Message) (err error) {
 	body := new(strings.Builder)
 
 	// Write mail header
@@ -104,20 +105,23 @@ func (p ConsoleProvider) send(msg Message) error {
 	_, _ = fmt.Fprint(body, "\r\n")
 
 	if mixedW != nil {
-		if _, err := mixedW.CreatePart(textproto.MIMEHeader{
+		if _, err = mixedW.CreatePart(textproto.MIMEHeader{
 			"Content-Type": {"multipart/alternative", "boundary=" + altW.Boundary()},
 		}); err != nil {
 			return errors.Wrap(err, "creating multipart/alternative part")
 		}
 	}
 
-	w, err := altW.CreatePart(textproto.MIMEHeader{"Content-Type": {contentTypeText}})
-	if err != nil {
-		return errors.Wrap(err, "creating text/plain part")
+	var w io.Writer
+	if msg.TextContent != "" {
+		w, err = altW.CreatePart(textproto.MIMEHeader{"Content-Type": {contentTypeText}})
+		if err != nil {
+			return errors.Wrap(err, "creating text/plain part")
+		}
+		_, _ = fmt.Fprintf(w, "%s\r\n", msg.TextContent)
 	}
-	_, _ = fmt.Fprintf(w, "%s\r\n", msg.TextContent)
 
-	if msg.TemplateName != "" {
+	if msg.HTMLContent != "" {
 		w, err = altW.CreatePart(textproto.MIMEHeader{"Content-Type": {contentTypeHTML}})
 		if err != nil {
 			return errors.Wrap(err, "creating text/html part")
