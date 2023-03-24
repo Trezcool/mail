213cb10ed252ba94f55c309ef5a4a27b31454351
Tresor Kambembo <tresor@propertyfinder.ae>
2023-03-24 10:19:55 +0400

    skeleton for other providers

--- mailchimp.go
+++ mailchimp.go
@@ -5,7 +5,7 @@ type MailChimpProvider struct {
 	baseProvider
 }
 
-func (p *MailChimpProvider) send(msg Message) (err error) {
+func (p *MailChimpProvider) send(msg Message) error {
 	// TODO: implement me
 	panic("implement me")
 }
--- mailgun.go
+++ mailgun.go
@@ -5,7 +5,7 @@ type MailGunProvider struct {
 	baseProvider
 }
 
-func (p *MailGunProvider) send(msg Message) (err error) {
+func (p *MailGunProvider) send(msg Message) error {
 	// TODO: implement me
 	panic("implement me")
 }
